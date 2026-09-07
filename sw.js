fb5b1b091735f75f2e66e997bd2acfa3f5ad42da
JimHess <jhess77@gmail.com>
2026-09-07 17:14:01 -0400

    Publish Rally PWA for GitHub Pages

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("rally-v1").then((cache) => cache.addAll(["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon.svg"]))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request).then((res) => res).catch(() => caches.match("./index.html")))
+  );
+});
